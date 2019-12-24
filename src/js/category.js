2392db157a5277e842d6ba18c702171813d80c2f
Felix-lx <417732673@qq.com>
2019-12-24 12:04:59 +0800

    比价查询完成

---
+++ src/js/category.js
@@ -0,0 +1,27 @@
+$(function(){
+    let titleId
+    // 一级模板引擎的ajax请求
+    $.ajax({
+        type:'get',
+        url:'http://localhost:3000/api/getcategorytitle',
+        success:function(info){
+            $('.category-title').html(template('tpl',info))
+            }
+        })
+
+    // 二级模板引擎的ajax请求
+    function secondContent(titleId) {
+        $.ajax({
+            type:'get',
+            url:'http://localhost:3000/api/getcategory?titleid='+ titleId,
+            success:function(info2){
+                $('#'+titleId).html(template('tpl2',info2)) 
+            }
+        })
+    }
+    // 点击一级菜单触发二级模板引擎发送请求
+    $('.category-title').on('click','li',function(){
+        titleId = $(this).data('titleid')
+        $(this).children('ul').toggleClass('hidden').hasClass('hidden')? '':secondContent(titleId)
+    })
+})
