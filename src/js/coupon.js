3ddfe0f1c58839b869dedc4d16b1c33f4388c19b
Felix-lx <417732673@qq.com>
2019-12-26 01:24:42 +0800

    coupon页面完成+头部的细节更改

---
+++ src/js/coupon.js
@@ -0,0 +1,10 @@
+$(function(){
+    $.ajax({
+        type:'get',
+        url:'http://localhost:3000/api/getcoupon',
+        success:function(info){
+            console.log(info)
+            $('.coupon-list').html(template('tpl',info))
+        }
+    })
+})
